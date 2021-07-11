2e1714a21900f54b0595b7ed07487347efa3aedd
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-07-11 19:46:04 +0100

    Start building iOS application

---
+++ PredictiveHeartDiseaseApp/PredictiveHeartDiseaseApp/Model/Target.swift
@@ -0,0 +1,15 @@
+//
+//  Target.swift
+//  PredictiveHeartDiseaseApp
+//
+//  Created by Forum Desai on 11/07/2021.
+//  Copyright © 2021 Forum Desai. All rights reserved.
+//
+
+import UIKit
+
+struct Target {
+    let value: Int
+    let advice: String
+    let colour: UIColor
+}
